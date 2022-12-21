bf97febe9a726a199534b9dd6e0e29b8e9813ee1
Daniil Davidovich <dd.jopgm@gmail.com>
2022-12-21 03:50:51 +0300

    Marks, and basic setting view

--- Home Work 11/Sources/ViewController.swift
+++ Home Work 11/Sources/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - UI Elements
+    
+    
+    
+    //MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
+    
+    //MARK: - Setups
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    
+    //MARK: - Action
+    
 
 
 }
